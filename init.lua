1136b1992ae7b155d609ee2e77396ce60fd85fa0
Ivan Yeung <ivanyeung0610@gmail.com>
2025-06-04 00:06:25 -0400

    Added Neotree plugin

--- init.lua
+++ init.lua
@@ -289,6 +289,23 @@ require('lazy').setup({
     },
   },
 
+  { -- Neo-tree plugin
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+      'MunifTanjim/nui.nvim',
+      -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    lazy = false, -- neo-tree will lazily load itself
+    ---@module "neo-tree"
+    ---@type neotree.Config?
+    opts = {
+      -- fill any relevant options here
+    },
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
